d534611d5c30649326a1f3a01816e7e743a48071
wesleypires <wesleypiresads@gmail.com>
2022-01-17 10:52:33 -0300

    Criando as variáveis e funções de interação com documento HTML

---
+++ script.js
@@ -0,0 +1,60 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let aviso = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = false;
+let votos = [];
+
+function iniciarEtapas() {
+	let etapa = etapas[etapaAtual];
+	let numeroHTML = '';
+	numero = '';
+	votoBranco = false;
+
+	for ( let i = 0; i < etapa.numeros; i++ ) {
+		if ( i == 0 ) {
+			numeroHTML += '<div class="numero pisca"></div>';
+		} else {
+			numeroHTML += '<div class="numero"></div>';
+		}
+	}
+
+	seuVotoPara.style.display = 'none';
+	cargo.innerHTML = etapa.titulo;
+	descricao.innerHTML = '';
+	aviso.style.display = 'none';
+	lateral.innerHTML = '';
+	numeros.innerHTML = numeroHTML;
+}
+
+function atualizaInterface() {
+	let etapa = etapas[etapaAtual];
+	let candidato = etapa.candidatos.filter((item) => {
+		if ( item.numero === numero ) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+	if ( candidato.length > 0 ) {
+		candidato = candidato[0];
+		seuVotoPara.style.display = 'block';
+		aviso.style.display = 'block';
+		descricao.innerHTML = `Nome: ${candidato.nome}</br> Partido: ${candidato.partido}`;
+
+		let fotosHTML = '';
+		for ( let i in candidato.fotos ) {
+			fotosHTML += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+		}
+		lateral.innerHTML = fotosHTML;
+	} else {
+		seuVotoPara.style.display = 'block';
+		aviso.style.display = 'block';
+		descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+	}
+}
