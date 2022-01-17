c499d14439e38a4271237b609ae35ca0a867f986
wesleypires <wesleypiresads@gmail.com>
2022-01-17 10:13:53 -0300

    Criação e configuração do arquivo no formato JSON para cadastrar os candidatos

---
+++ etapas.js
@@ -0,0 +1,52 @@
+// Arquivo formato JSON
+
+let etapas = [
+	{
+		titulo: 'VEREADOR'.
+		numeros: 5,
+		candidatos: [
+			{
+				numero: '10000',
+				nome: 'Fulano de tal',
+				partido: 'ABCD',
+				foto: [
+					{url: 'foto.jpg', leganda: 'Vereador'}
+				]
+			},
+			{
+				numero: '20000',
+				nome: 'Beltrano de tal',
+				partido: 'EFGH',
+				foto: [
+					{url: 'foto.jpg', leganda: 'Vereador'}
+				]
+			},
+		]
+	},
+	{
+		titulo: 'PREFEITO',
+		numeros: '2',
+		candidatos: [
+			{
+				numero: '10',
+				nome: 'Allien Vicentino',
+				partido: 'HPP',
+				vice: 'Predador',
+				fotos: [
+					{url: 'foto.jpg', leganda: 'Prefeito'},
+					{url: 'foto.jpg', leganda: 'Vice-Prefeito', small: true}
+				]
+			},
+			{
+				numero: '20',
+				nome: 'Rabito Silver',
+				partido: 'PPS',
+				vice: 'Codar',
+				fotos: [
+					{url: 'foto.jpg', leganda: 'Prefeito'},
+					{url: 'foto.jpg', leganda: 'Vice-Prefeito', small: true}
+				]
+			},
+		]
+	}
+];
